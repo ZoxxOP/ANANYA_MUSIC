82fca4736bf0f88072ecaa100ac7240d598addfa
𝐀𝐤𝐚𝐬𝐡 𝐃𝐚𝐤𝐬𝐡𝐰𝐚𝐧𝐬𝐡𝐢 : ZoxxOP <akp954834@gmail.com>
2025-12-05 20:05:22 +0530

    Update Youtube.py

--- ANANYA_MUSIC/platforms/Youtube.py
+++ ANANYA_MUSIC/platforms/Youtube.py
@@ -7,7 +7,7 @@ import requests
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from youtubesearchpython import VideosSearch
+from py_yt import VideosSearch
 from ANANYA_MUSIC.utils.database import is_on_off
 from ANANYA_MUSIC import app
 from ANANYA_MUSIC.utils.formatters import time_to_seconds
